3227954f8ac22cecd9e3a6c746bbbd9788ca3adc
ImreSamu <samui@pella.hu>
2013-08-16 15:41:37 +0200

    re-tested ...

--- install_postgis.sh
+++ install_postgis.sh
@@ -3,23 +3,24 @@
 DB_NAME=osm
 
 GEOSVERSION=3.4.0
-POSTGIS_VERSION=$POSTGIS_VERSION
+POSTGIS_VERSION=2.1.0rc2
 PG_VERSION=9.3
-GDAL_VERSION=1.9.2
+GDALVERSION=1.9.2
 SRC_DIR=/home/
 
 
 sudo /etc/init.d/postgresql stop
 
-function upgrade_postgis { 
+function upgrade_postgis {
 
 # install any missing prerequisites
-sudo apt-get install -y  gdebi build-essential checkinstall  \
-  postgresql-server-dev-9.3 libjson0-dev libxml2-dev libproj-dev \
-  python2.7-dev swig binutils
- 
-mkdir -p $SRC_DIR
+sudo  apt-get install -y gdebi build-essential checkinstall
+sudo  apt-get install -y postgresql-server-dev-9.3 libjson0-dev libxml2-dev libproj-devs
+sudo  apt-get install -y python2.7-dev swig binutils
 
+
+function install_geos {
+mkdir -p $SRC_DIR
 echo "Downloading and compiloing GEOS" 
 # download and compile geos in /opt/geos
 cd $SRC_DIR
@@ -30,7 +31,10 @@ cd geos-$GEOSVERSION/
 make -j2
 sudo checkinstall  # uninstall with: dpkg -r geos
 sudo gdebi geos_$GEOSVERSION-1_amd64.deb
+}
 
+
+function install_gdal {
 echo "Downloading and compiloing GDAL" 
 # download and compile gdal in /opt/gdal
 cd $SRC_DIR
@@ -41,12 +45,15 @@ cd gdal-$GDALVERSION/
   --with-pg=/usr/lib/postgresql/9.3/bin/pg_config --with-python
 make -j2
 sudo checkinstall  # uninstall with: dpkg -r gdal
-sudo gdebi gdal_$GDAL_VERSION-1_amd64.deb
- 
+sudo gdebi gdal_$GDALVERSION-1_amd64.deb
+}
+
+
+function install_postgis {
 echo "Downloading and compiloing PostGIS"
 # download and compile postgis 2 in default location
 cd $SRC_DIR
-wget http://www.postgis.org/download/postgis-$POSTGIS_VERSION.tar.gz
+wget http://download.osgeo.org/postgis/source/postgis-$POSTGIS_VERSION.tar.gz
 tar xvzf postgis-$POSTGIS_VERSION.tar.gz
 cd postgis-$POSTGIS_VERSION/
 ./configure --with-geosconfig=/opt/geos/bin/geos-config \
@@ -54,7 +61,13 @@ cd postgis-$POSTGIS_VERSION/
 make -j2
 sudo checkinstall  # uninstall with: dpkg -r postgis
 sudo gdebi postgis_$POSTGIS_VERSION-1_amd64
- 
+}
+
+
+install_geos
+install_gdal
+install_postgis
+
 # for command-line tools, append this line to .profile/.bashrc/etc.
 export PATH=$PATH:/opt/geos/bin:/opt/gdal/bin
  
@@ -69,18 +82,27 @@ sudo ldconfig
 sudo /etc/init.d/postgresql start
 }
 
+
+
+
 function restore_postgisDB {
-	# Migrate a previous PostGIS DB into the newly installed version
-	# supply the DB name to be used for the latest postgis DB and
-	# a file path to the pg_dump -Fc backup from an earlier PostGIS version
-	echo 'create database -E UTF8 $DB_NAME;' | sudo -u postgres psql
-	echo 'create extension postgis; create extension postgis_topology;' \
-	  | sudo -u postgres psql -d $DB_NAME
-	/usr/share/postgresql/$PG_VERSION/contrib/postgis-$PG_VERSION/postgis_restore.pl \
-	  /path/to/mydb.dump \
-	  | sudo -u postgres psql -d $DB_NAME
+        # Migrate a previous PostGIS DB into the newly installed version
+        # supply the DB name to be used for the latest postgis DB and
+        # a file path to the pg_dump -Fc backup from an earlier PostGIS version
+        echo 'create database -E UTF8 $DB_NAME;' | sudo -u postgres psql
+        echo 'create extension postgis; create extension postgis_topology;' \
+          | sudo -u postgres psql -d $DB_NAME
+        /usr/share/postgresql/$PG_VERSION/contrib/postgis-$PG_VERSION/postgis_restore.pl \
+          /path/to/mydb.dump \
+          | sudo -u postgres psql -d $DB_NAME
 }
 
 
 
 upgrade_postgis
+
+
+
+
+
+
